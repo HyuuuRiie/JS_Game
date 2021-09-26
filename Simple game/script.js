f5eeefda3320d2728cedbc7558a0ecd741033a22
HyuuuRiie <wahyuseptiadifajri11@gmail.com>
2021-09-26 15:32:20 +0700

    masih gak work lompat nya :(

--- Simple game/script.js
+++ Simple game/script.js
@@ -2,9 +2,19 @@ var character = document.getElementById("character");
 var block = document.getElementById("block");
 
 function jump() {
-	if (character) {}
-	character.classList.add("animate");
+	if (character.classList != "animate"){
+		character.classList.add("animate");
+	}
 	setTimeout(function(){
 		character.classList.remove("animate");
 	},500);
 }
+var checkDead = setInterval(function(){
+	var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+	var blockleft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+	if (blockleft<20 && blockleft>0 && characterTop >=130) {
+		block.style.animation = "none";
+		block.style.display = "none";
+		alert("U lose.");
+	}
+},10);
